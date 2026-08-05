f971c1a4f25126f8973bbfa45d71b5cd8245dc23
Bereket Sime <bereketsime777@gmail.com>
2026-08-05 11:47:48 +0000

    Debug Vite API URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,16 +1,46 @@
 import axios from "axios";
 
+console.log(
+  "VITE API URL:",
+  import.meta.env.VITE_API_URL
+);
+
+
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
+
+  baseURL:
+    import.meta.env.VITE_API_URL ||
+    "http://localhost:5000/api",
+
 });
 
-// Automatically attach token to every request
+
+
 API.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
+
+
+  const token =
+    localStorage.getItem("token");
+
+
   if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+
+    config.headers.Authorization =
+      `Bearer ${token}`;
+
   }
+
+
+  console.log(
+    "Calling API:",
+    config.baseURL + config.url
+  );
+
+
   return config;
+
+
 });
+
 
 export default API;
