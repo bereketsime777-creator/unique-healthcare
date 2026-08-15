b3e31b5c72b8c57beea40ccafaa01f966ffcb065
Bereket Sime <bereketsime777@gmail.com>
2026-08-15 10:04:28 +0000

    Update app configuration and remove unused ChangePassword page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,7 +12,6 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import ForgotPassword from "./pages/ForgotPassword";
 import ResetPassword from "./pages/ResetPassword";
-import ChangePassword from "./pages/ChangePassword";
 import Cart from "./pages/Cart";
 import Checkout from "./pages/Checkout";
 import PaymentSuccess from "./pages/PaymentSuccess";
@@ -74,7 +73,6 @@ function Layout() {
         <Route path="/register" element={<Register />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/reset-password/:token" element={<ResetPassword />} />
-        <Route path="/change-password" element={<ChangePassword />} />
 
         {/* Admin */}
         <Route
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const { protect } = require("../middleware/authMiddleware");
 
 const {
   register,
@@ -8,7 +7,6 @@ const {
   forgotPassword,
   resetPassword,
   verifyResetToken,
-  changePassword,
 } = require("../controllers/authController");
 
 router.post("/register", register);
@@ -16,6 +14,5 @@ router.post("/login", login);
 router.post("/forgot-password", forgotPassword);
 router.post("/reset-password/:token", resetPassword);
 router.get("/verify-reset-token/:token", verifyResetToken);
-router.post("/change-password", protect, changePassword);
 
 module.exports = router;
