791ce23439a438d02368b13ec6a7aa74e3a211b0
Bereket Sime <bereketsime777@gmail.com>
2026-09-10 16:08:59 +0000

    update na

--- client/src/pages/Products.jsx
+++ client/src/pages/Products.jsx
@@ -313,7 +313,7 @@ function Products() {
                           <img
                             src={product.image}
                             alt={product.name}
-                            className="h-40 object-contain group-hover:scale-105 transition-transform duration-300"
+                            className="w-full h-full object-cover group-hover:scale-105 transition-transform duration-300"
                             onError={(e) => {
                               e.target.style.display = "none";
                               e.target.nextSibling.style.display = "flex";
